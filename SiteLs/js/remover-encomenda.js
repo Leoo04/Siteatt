8ddbe1ad93df2827025a56bb6a369da5245445fe
Leoo04 <147108380+Leoo04@users.noreply.github.com>
2024-06-06 22:17:37 -0300

    Add files via upload

---
+++ SiteLs/js/remover-encomenda.js
@@ -0,0 +1,11 @@
+var tabela = document.querySelector("table");
+
+tabela.addEventListener("dblclick", function(event){
+    event.target.parentNode.classList.add("fadeOut");
+    
+    setTimeout(function(){
+                //Captura a TR da TD que sofreu o duplo clique e remove
+                event.target.parentNode.remove();
+               }, 500);
+    
+});
